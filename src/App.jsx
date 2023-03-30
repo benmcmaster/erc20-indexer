6b4ecca9a59dfb28492623976fc38da3550e3c7c
Ben McMaster <ben@benmcmaster.com>
2023-03-29 22:43:00 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,69 @@ import {
   Input,
   SimpleGrid,
   Text,
+  Stack,
+  Card,
+  CardBody,
 } from '@chakra-ui/react';
 import { Alchemy, Network, Utils } from 'alchemy-sdk';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 function App() {
   const [userAddress, setUserAddress] = useState('');
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function getTokenBalance() {
+  const { address, isConnected } = useAccount()
+  const account = useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      console.log('Connected', { address, connector, isReconnected });
+      setUserAddress(address);
+      setResults([]);
+      setHasQueried(false);
+      // getTokenBalance(null, address, "onConnect");
+    },
+    onDisconnect() {
+      console.log('Disconnected')
+      setUserAddress("");
+      setResults([]);
+      setHasQueried(false);
+    },
+  })
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
+
+  async function getTokenBalance(event, _address, from) {
+    setIsLoading(true);
+    setResults([]);
+    setTokenDataObjects([]);
+    setHasQueried(false);
+    console.log("Get Token Balance userAddress: ", userAddress);
+    console.log("Get Token Balance: _address: ", _address);
+    console.log("Get Token Balance: from: ", from);
+    console.log("Get Token Balance: event: ", event);
+    const callAddress = _address ? _address : userAddress;
+
+    const apiKey = "zzWHdoHRNQ_s3yaJVr-RMMAUlcgpbyyc";
+
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey,
       network: Network.ETH_MAINNET,
     };
 
     const alchemy = new Alchemy(config);
-    const data = await alchemy.core.getTokenBalances(userAddress);
+
+    const resolvedAddress = await alchemy.core.resolveName(callAddress);
+    console.log("Resolved Address: ", resolvedAddress);
+    const data = await alchemy.core.getTokenBalances(callAddress);
 
     setResults(data);
 
@@ -38,80 +83,120 @@ function App() {
       tokenDataPromises.push(tokenData);
     }
 
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
+    const tokenDataObjects = await Promise.all(tokenDataPromises);
+    console.log("Token Data Objects: ", tokenDataObjects);
+
+    setTokenDataObjects(tokenDataObjects);
     setHasQueried(true);
+    setIsLoading(false);
   }
   return (
-    <Box w="100vw">
-      <Center>
-        <Flex
-          alignItems={'center'}
-          justifyContent="center"
-          flexDirection={'column'}
-        >
-          <Heading mb={0} fontSize={36}>
-            ERC-20 Token Indexer
-          </Heading>
-          <Text>
-            Plug in an address and this website will return all of its ERC-20
-            token balances!
-          </Text>
-        </Flex>
-      </Center>
-      <Flex
-        w="100%"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent={'center'}
-      >
-        <Heading mt={42}>
-          Get all the ERC-20 token balances of this address:
-        </Heading>
-        <Input
-          onChange={(e) => setUserAddress(e.target.value)}
-          color="black"
-          w="600px"
-          textAlign="center"
-          p={4}
-          bgColor="white"
-          fontSize={24}
+    <Box>
+      <Box className='connect-button-container'>
+        <ConnectButton
+          label="Connect Wallet"
+          accountStatus="address"
+          chainStatus="icon"
+          showBalance={true}
         />
-        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor="blue">
-          Check ERC-20 Token Balances
-        </Button>
+      </Box>
+      <Box w="100vw" marginTop="100px">
+        <Center>
+          <Flex
+            alignItems={'center'}
+            justifyContent="center"
+            flexDirection={'column'}
+          >
+            <Heading mb={0} fontSize={36}>
+              ERC-20 Token Indexer
+            </Heading>
+            <Text>
+              Plug in an address and this website will return all of its ERC-20
+              token balances!
+            </Text>
+          </Flex>
+        </Center>
+        <Flex
+          w="100%"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent={'center'}
+        >
+          <Heading mt={42}>
+            Get all the ERC-20 token balances of this address:
+          </Heading>
+          <Input
+            value={userAddress}
+            onChange={(e) => setUserAddress(e.target.value)}
+            color="black"
+            w="600px"
+            textAlign="center"
+            p={4}
+            bgColor="white"
+            fontSize={16}
+            mt={12}
+          />
+          <Button
+            fontSize={20}
+            onClick={getTokenBalance}
+            colorScheme='teal'
+            isLoading={isLoading}
+            loadingText='Checking'
+            mt={12}
+          >
+            Check ERC-20 Token Balances
+          </Button>
 
-        <Heading my={36}>ERC-20 token balances:</Heading>
+          <Heading my={10}>ERC-20 token balances:</Heading>
 
-        {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
-            {results.tokenBalances.map((e, i) => {
-              return (
-                <Flex
-                  flexDir={'column'}
-                  color="white"
-                  bg="blue"
-                  w={'20vw'}
-                  key={e.id}
-                >
-                  <Box>
-                    <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
-                  </Box>
-                  <Box>
-                    <b>Balance:</b>&nbsp;
-                    {Utils.formatUnits(
-                      e.tokenBalance,
-                      tokenDataObjects[i].decimals
-                    )}
-                  </Box>
-                  <Image src={tokenDataObjects[i].logo} />
-                </Flex>
-              );
-            })}
-          </SimpleGrid>
-        ) : (
-          'Please make a query! This may take a few seconds...'
-        )}
-      </Flex>
+          {hasQueried ? (
+            <SimpleGrid w={'90vw'} columns={4} spacing={10}>
+              {results.tokenBalances.map((e, i) => {
+                return (
+                  // <Flex
+                  //   flexDir={'column'}
+                  //   color="white"
+                  //   bg="blue"
+                  //   w={'20vw'}
+                  //   key={i}
+                  // >
+                  //   <Box>
+                  //     <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
+                  //   </Box>
+                  //   <Box>
+                  //     <b>Balance:</b>&nbsp;
+                  //     {Utils.formatUnits(
+                  //       e.tokenBalance,
+                  //       tokenDataObjects[i].decimals
+                  //     )}
+                  //   </Box>
+                  //   <Image src={tokenDataObjects[i].logo} />
+                  // </Flex>
+                  <Card maxW='sm'>
+                    <CardBody>
+                      <Image
+                        src={tokenDataObjects[i].logo}
+                        alt='Green double couch with wooden legs'
+                        borderRadius='lg'
+                      />
+                      <Stack mt='6' spacing='3'>
+                        <Heading size='md'>
+                          {tokenDataObjects[i].symbol}
+                        </Heading>
+                        <Text color='blue.600'>
+                          {Utils.formatUnits(e.tokenBalance, tokenDataObjects[i].decimals)}
+                        </Text>
+                      </Stack>
+                    </CardBody>
+                  </Card>
+                );
+              })}
+            </SimpleGrid>
+          ) : (
+            'Please make a query! This may take a few seconds...'
+          )}
+        </Flex>
+      </Box>
     </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+const apiKey = "zzWHdoHRNQ_s3yaJVr-RMMAUlcgpbyyc";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    alchemyProvider({ apiKey }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} coolMode >  
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>,
 )
